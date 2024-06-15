31b5bfafe8994b752b643b148d265ebe363ac647
msony552@gmail.com <msony552@gmail.com>
2024-06-15 14:00:41 +0530

    added test cases for calculateSum

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import add from '../src/index'
+import add, { calculateSum } from '../src/index';
 
 describe('add', () => {
     it('should return 0', () => {
@@ -11,5 +11,19 @@ describe('add', () => {
 
     it('should return sum of n numbers', () => {
         expect(add('1,2,3,4,5,6,7,8,9,10')).toBe(55);
+    });
+});
+
+describe('calculateSum', () => {
+    it('should return 0', () => {
+        expect(calculateSum([])).toBe(0);
+    });
+
+    it('should return 3', () => {
+        expect(calculateSum([1, 2])).toBe(3);
+    });
+
+    it('should return sum of n numbers', () => {
+        expect(calculateSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(55);
     });
 });
