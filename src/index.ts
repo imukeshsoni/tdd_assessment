628e8bafbf3070067c719e26ab2a5df0469811d6
msony552@gmail.com <msony552@gmail.com>
2024-06-15 13:21:54 +0530

    updated calculator to pass the sample test case

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export default function calculator()  {
-    return 'Hello, world!';
+export default function calculator(nums: string): number {
+    const [a, b] = nums.split(',').map(Number);
+    return a + b;
 }
