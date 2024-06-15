bee2c50a0ac38a25ea7b15646d60fb32f267978c
msony552@gmail.com <msony552@gmail.com>
2024-06-15 13:58:15 +0530

    added test for calculating sum of n numbers

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -8,4 +8,8 @@ describe('add', () => {
     it('should return 3', () => {
         expect(add('1,2')).toBe(3);
     });
+
+    it('should return sum of n numbers', () => {
+        expect(add('1,2,3,4,5,6,7,8,9,10')).toBe(55);
+    });
 });
