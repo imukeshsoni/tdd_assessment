0c1104f787f590ec25fb8e8bea9e76fb2010fef7
msony552@gmail.com <msony552@gmail.com>
2024-06-15 13:23:42 +0530

    added test to return 0 for empty string

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,10 @@
 import calculator  from '../src/index'
 
 describe('calculator', () => {
+    it('should return 0', () => {
+        expect(calculator('')).toBe(0);
+    });
+
     it('should return 3', () => {
         expect(calculator('1,2')).toBe(3);
     });
